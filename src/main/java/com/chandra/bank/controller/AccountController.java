8299db0f69b1adb85fb7d3eb6ff0bd3aaddf2909
Jishnu Janardhanan <>
2024-03-03 13:35:37 +0530

    First Commit

---
+++ src/main/java/com/chandra/bank/controller/AccountController.java
@@ -0,0 +1,21 @@
+package com.chandra.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    //Create An Account
+
+    //Get An Account
+
+    //Activate An Account
+
+    //Modify An Account
+
+    //Delete An Account
+
+}
